996f12d2634d9428832d9c03573cb4c523737aeb
Matteo Cossu <matteo.cossu@gmail.com>
2022-02-23 17:26:40 -0800

    v5

--- 03_12/Bookmc.js
+++ 03_12/Bookmc.js
@@ -35,3 +35,37 @@ class Book {
 }
 
 export default Book;
+
+class Backpack {
+  constructor(
+    // Defines parameters:
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen
+  ) {
+    // Define properties:
+    this.name = name;
+    this.volume = volume;
+    this.color = color;
+    this.pocketNum = pocketNum;
+    this.strapLength = {
+      left: strapLengthL,
+      right: strapLengthR,
+    };
+    this.lidOpen = lidOpen;
+  }
+  // Add methods like normal functions:
+  toggleLid(lidStatus) {
+    this.lidOpen = lidStatus;
+  }
+  newStrapLength(lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  }
+}
+
+export default Backpack;
--- 03_12/script.js
+++ 03_12/script.js
@@ -16,5 +16,21 @@ const encyclopedia = new Book(
   "February 23, 1922 15:00:00 PST"
 );
 
+import Backpack from "./Backpack.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false,
+  "December 5, 2018 15:00:00 PST"
+);
+
 console.log("The Encyclopedia object:", encyclopedia);
 console.log("Date Published:", encyclopedia.datePublished);
+
+console.log("The everydayPack object:", everydayPack);
+console.log("Date acquired:", everydayPack.dateAcquired);
